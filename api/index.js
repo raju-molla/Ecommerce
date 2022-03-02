00c929197bfec35d6d1fe7cdab9b88435f010314
raju-molla <iamraju.eu56@gmail.com>
2022-03-02 23:56:24 +0600

    backend

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-mongoose.connect("mongodb+srv://ecommerce:01703634507@cluster0.66f2j.mongodb.net/shop?retryWrites=true&w=majority").then(()=> console.log("DB Connection successfully")).catch((err)=>{
+mongoose.connect(process.env.MONGO_URL).then(()=> console.log("DB Connection successfully")).catch((err)=>{
     console.log(err);
 })
 
 
-app.listen(5000, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("backend server si running");
 })
